d81309b79c1785bc70a4f7c6d94588dd92a6f650
AsoTiesto <cute17852337569@gmail.com>
2018-10-08 14:54:52 +0800

    新增功能：製作學測及指考qrcode

---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+const Q = require('q')
+const fs = require('fs')
+const del = require('del')
+const gulp = require('gulp')
+const util = require('gulp-template-util')
+const gcPub = require('gulp-gcloud-publish')
+
+const bucketNameForTest = 'tutor-events-test'
+const bucketNameForProd = 'tutor-events'
+const projectId = 'tutor-204108'
+const keyFileName = 'tutor.json'
+const projectName = 'event/make-qrcode/'
+
+let libTask = dest => {
+  return function () {
+    var packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8').toString())
+    if (!packageJson.dependencies) {
+      packageJson.dependencies = {}
+    }
+    var webLibModules = []
+    for (var module in packageJson.dependencies) {
+      webLibModules.push('node_modules/' + module + '/**/*')
+    }
+    return gulp.src(webLibModules, {
+      base: 'node_modules/'
+    })
+      .pipe(gulp.dest(dest))
+  }
+}
+
+let copyStaticTask = dest => {
+  return function () {
+    return gulp.src([
+      'src/*.html',
+      'src/img/**',
+      'src/css/*.css',
+      'src/js/**/*.js',
+      'src/lib/*.js',
+      'src/js/module-utils/*.js'
+    ], {
+      base: 'src'
+    })
+      .pipe(gulp.dest(dest))
+  }
+}
+
+let cleanTask = () => {
+  return del(['dist', ''])
+}
+
+let uploadGCS = bucketName => {
+  return gulp
+    .src([
+      './dist/*.html',
+      './dist/js/*.js',
+      './dist/lib/*.js'
+    ], {
+      base: `${__dirname}/dist/`
+    })
+    .pipe(gcPub({
+      bucket: bucketName,
+      keyFilename: keyFileName,
+      base: projectName,
+      projectId: projectId,
+      public: true,
+      metadata: {
+        cacheControl: 'private, no-transform'
+      }
+    }))
+}
+
+gulp.task('uploadGcpTest', uploadGCS.bind(uploadGCS, bucketNameForTest))
+gulp.task('uploadGcpProd', uploadGCS.bind(uploadGCS, bucketNameForProd))
+gulp.task('clean', cleanTask)
+gulp.task('lib', libTask('src/lib'))
+
+gulp.task('package', () => {
+  let deferred = Q.defer()
+  Q.fcall(() => {
+    return util.logPromise(cleanTask)
+  }).then(() => {
+    return Q.all([
+      util.logStream(copyStaticTask('dist'))
+    ])
+  })
+
+  return deferred.promise
+})
---
+++ src/js/make-qrcode.js
@@ -0,0 +1,60 @@
+(function () {
+  let url = window.location.search
+  let ehanlinHost = `https://www.ehanlin.com.tw/event/collection/?`
+  let year = url.split('&')[0].split('=')[1]
+  let type = url.split('&')[1].split('=')[1]
+  let htmlType = decodeURI(type)
+  let subject = url.split('&')[2].split('=')[1]
+  let typeIdx = url.split('&')[3].split('=')[1]
+  let dataCode
+  let idx = typeIdx
+
+  for (let index = 1; index <= idx; index++) {
+    $('#qrcode').append(
+      `
+      <div id="qrcode${index}">
+        <div data-title="${type}科目${subject}第${index}題">${htmlType} 第${index}題 科目${subject}</div>
+        <a id="downloadLink"></a>
+        <button type="button">下载</button>
+      </div>
+      <hr>
+      `
+    )
+    dataCode = {
+      text: `${ehanlinHost}year=${year}&type=${type}&subject=${subject}#1_${index}`,
+      width: 128,
+      height: 128,
+      colorDark: '#000000',
+      colorLight: '#ffffff',
+      correctLevel: QRCode.CorrectLevel.H
+    }
+    let qrcode = new QRCode(document.getElementById(`qrcode${index}`), dataCode)
+  }
+
+  let downloadClick = (index, qrcodeName) => {
+    let img = document.getElementById('qrcode').getElementsByTagName('img')[index - 1]
+    let canvas = document.createElement('canvas')
+
+    canvas.width = img.width
+    canvas.height = img.height
+    canvas.getContext('2d').drawImage(img, 0, 0)
+
+    let url = canvas.toDataURL('image/jpg')
+    let downloadLink = document.getElementById('downloadLink')
+
+    downloadLink.setAttribute('href', url)
+    downloadLink.setAttribute('download', `${qrcodeName}.jpg`)
+    downloadLink.click()
+  }
+
+  for (let index = 1; index <= idx; index++) {
+    $(`#qrcode${index} button`).on('click', () => {
+      console.log(index)
+      let qrcodeName = $(`#qrcode${index} div`).attr('data-title')
+      let typeName = decodeURI(qrcodeName)
+
+      console.log(qrcodeName)
+      downloadClick(index, typeName)
+    })
+  }
+})()

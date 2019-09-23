7e3d7bf29d50acac30528459dba6cbdc189b9386
AsoTiesto <cute17852337569@gmail.com>
2019-09-23 18:44:00 +0800

    調整功能：調整 make-qrc gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,77 @@
-const Q = require('q')
-const fs = require('fs')
-const del = require('del')
-const gulp = require('gulp')
-const util = require('gulp-template-util')
-const gcPub = require('gulp-gcloud-publish')
+const Q = require("q");
+const fs = require("fs");
+const del = require("del");
+const gulp = require("gulp");
+const util = require("gulp-template-util");
+const gcPub = require("gulp-gcloud-publish");
 
-const bucketNameForTest = 'tutor-apps-test'
-const bucketNameForProd = 'tutor-apps'
-const projectId = 'tutor-204108'
-const keyFileName = 'tutor.json'
-const projectName = 'app/make-qrcode/'
+const bucketNameForTest = "tutor-test-apps";
+const projectIdTest = "tutor-test-238709";
+const keyFileNameTest = "tutor-test.json";
+const projectName = "app/make-qrcode/";
 
 let libTask = dest => {
-  return function () {
-    var packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8').toString())
-    if (!packageJson.dependencies) {
-      packageJson.dependencies = {}
-    }
-    var webLibModules = []
-    for (var module in packageJson.dependencies) {
-      webLibModules.push('node_modules/' + module + '/**/*')
-    }
-    return gulp.src(webLibModules, {
-      base: 'node_modules/'
-    })
-      .pipe(gulp.dest(dest))
-  }
-}
+    return function() {
+        var packageJson = JSON.parse(fs.readFileSync("package.json", "utf8").toString());
+        if (!packageJson.dependencies) {
+            packageJson.dependencies = {};
+        }
+        var webLibModules = [];
+        for (var module in packageJson.dependencies) {
+            webLibModules.push("node_modules/" + module + "/**/*");
+        }
+        return gulp
+            .src(webLibModules, {
+                base: "node_modules/"
+            })
+            .pipe(gulp.dest(dest));
+    };
+};
 
 let copyStaticTask = dest => {
-  return function () {
-    return gulp.src([
-      'src/*.html',
-      'src/img/**',
-      'src/css/*.css',
-      'src/js/**/*.js',
-      'src/lib/*.js',
-      'src/js/module-utils/*.js'
-    ], {
-      base: 'src'
-    })
-      .pipe(gulp.dest(dest))
-  }
-}
+    return function() {
+        return gulp
+            .src(["src/*.html", "src/img/**", "src/css/*.css", "src/js/**/*.js", "src/lib/*.js", "src/js/module-utils/*.js"], {
+                base: "src"
+            })
+            .pipe(gulp.dest(dest));
+    };
+};
 
 let cleanTask = () => {
-  return del(['dist', ''])
-}
+    return del(["dist", ""]);
+};
 
-let uploadGCS = bucketName => {
-  return gulp
-    .src([
-      './dist/*.html',
-      './dist/js/*.js',
-      './dist/lib/*.js'
-    ], {
-      base: `${__dirname}/dist/`
-    })
-    .pipe(gcPub({
-      bucket: bucketName,
-      keyFilename: keyFileName,
-      base: projectName,
-      projectId: projectId,
-      public: true,
-      metadata: {
-        cacheControl: 'no-store'
-      }
-    }))
-}
+let uploadGCSTest = bucketName => {
+    return gulp
+        .src(["dist/*.html", "dist/js/**", "dist/lib/**"], {
+            base: `${__dirname}/dist/`
+        })
+        .pipe(
+            gcPub({
+                bucket: bucketName,
+                keyFilename: keyFileNameTest,
+                base: projectName,
+                projectId: projectIdTest,
+                public: true,
+                metadata: {
+                    cacheControl: "no-store"
+                }
+            })
+        );
+};
 
-gulp.task('uploadGcpTest', uploadGCS.bind(uploadGCS, bucketNameForTest))
-gulp.task('uploadGcpProd', uploadGCS.bind(uploadGCS, bucketNameForProd))
-gulp.task('clean', cleanTask)
-gulp.task('lib', libTask('src/lib'))
+gulp.task("uploadGcsTest", uploadGCSTest.bind(uploadGCSTest, bucketNameForTest));
+gulp.task("clean", cleanTask);
+gulp.task("lib", libTask("src/lib"));
 
-gulp.task('package', () => {
-  let deferred = Q.defer()
-  Q.fcall(() => {
-    return util.logPromise(cleanTask)
-  }).then(() => {
-    return Q.all([
-      util.logStream(copyStaticTask('dist'))
-    ])
-  })
+gulp.task("package", () => {
+    let deferred = Q.defer();
+    Q.fcall(() => {
+        return util.logPromise(cleanTask);
+    }).then(() => {
+        return Q.all([util.logStream(copyStaticTask("dist"))]);
+    });
 
-  return deferred.promise
-})
+    return deferred.promise;
+});
